a38379c7aae7f101cbd72665df72adad7511804f
VBulykov <bulykov2013@mail.ru>
2025-11-07 13:00:04 +0300

    feat: добавлены миграции бд, создана начальная миграция

--- src/database/init_db.py
+++ src/database/init_db.py
@@ -1,9 +1,5 @@
-from src.database.base import Base
-from src.database.engine import engine
-# from src.models
+from src.database.migration_runner import apply_migrations
 
 
 async def init_db():
-    async with engine.begin() as conn:
-        # await conn.run_sync(Base.metadata.drop_all)  # только на dev
-        await conn.run_sync(Base.metadata.create_all)
+    await apply_migrations()
